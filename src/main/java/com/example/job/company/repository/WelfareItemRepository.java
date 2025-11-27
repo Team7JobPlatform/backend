44a4648eb0b2a486e713b446ec94e3da588f6b12
Yoonjongho1122 <k6k2dao1122@gmail.com>
2025-11-28 04:08:20 +0900

    작업내용

---
+++ src/main/java/com/example/job/company/repository/WelfareItemRepository.java
@@ -0,0 +1,12 @@
+package com.example.job.company.repository;
+
+import com.example.job.company.entity.WelfareItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WelfareItemRepository extends JpaRepository<WelfareItem, Long> {
+    List<WelfareItem> findByCategory(String category);
+}
